8c47a1b4bbb7b1c87cc63b8395674d1c5f03936f
johnsonakpama <johnsonakpama33@gmail.com>
2024-07-21 06:23:54 +0100

    My project almost there

---
+++ scripts/getdate.js
@@ -0,0 +1,23 @@
+const msinnav = document.querySelector('.nav');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+
+const full = document.querySelector("#full");
+const year = document.querySelector("#year");
+const month = document.querySelector("#month");
+const day = document.querySelector("#day");
+const time = document.querySelector("#time");
+
+const today = new Date();
+
+full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
